27a4b1b4ba887dac1be49b30a1562428c36841e2
SERGEY <dmmytrenko@gmail.com>
2021-09-30 19:48:10 +0300

    Add models [Comments, Posts, Users, Groups]

---
+++ Comunicazione/Comunicazione.Data/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comunicazione.Data.Models
+{
+    class Comment
+    {
+        public Comment()
+        {
+            this.TaggedFriends = new List<User>();
+        }
+
+        public int CommentId { get; set; }
+        public User Author { get; set; }
+        public int AuthorId => this.Author.UserId;
+        public string Content { get; set; }
+        public DateTime DatePosted { get; set; }
+
+        public ICollection<User> TaggedFriends { get; set; }
+    }
+}
---
+++ Comunicazione/Comunicazione.Data/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comunicazione.Data.Models
+{
+    class User
+    {
+        public User()
+        {
+            this.Publications = new List<Publication>();
+            this.Friends = new List<User>();
+            this.Groups = new List<Group>();
+        }
+
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName => $"{this.Name} {this.Surname}";
+        public Gender Gender { get; set; }
+        public string Bio { get; set; }
+
+
+        // Navigation properties
+        public ICollection<Publication> Publications { get; set; }
+        public ICollection<User> Friends { get; set; }
+        public ICollection<Group> Groups { get; set; }
+    }
+    public enum Gender
+    {
+        male = 0,
+        female = 1
+    }
+}
