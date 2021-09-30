db7fa645c3dfe1c247d654b813ecb6c554b4c8b0
SERGEY <dmmytrenko@gmail.com>
2021-10-01 00:10:29 +0300

    create models

---
+++ src/ApplicationCore/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public User Author { get; set; }
+        public Publication Publication { get; set; } 
+        public string Content { get; set; }
+
+        // Navigation prop
+        public int AuthorId => this.Author.Id;
+        public int PublicationId => this.Publication.Id;
+    }
+}
