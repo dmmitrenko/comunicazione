baf25aa48187799bddd43beb654f99431dbcaf9a
Sergey Dmitrenko <dmmytrenko@gmail.com>
2021-10-19 14:57:01 +0300

    postService

--- src/Comunicazione.Infrastructure/Services/PostService.cs
+++ src/Comunicazione.Infrastructure/Services/PostService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Comunicazione.Core.Entities;
 using Comunicazione.Core.Repositories;
 using Comunicazione.Core.Services;
 
@@ -15,6 +16,16 @@ namespace Comunicazione.Infrastructure.Services
         public PostService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+
+        public void AddPost(int userId, Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
