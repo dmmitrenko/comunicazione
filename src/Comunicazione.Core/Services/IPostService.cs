6f26fd773b219d686cfc4bb42007418923377eee
Sergey Dmitrenko <dmmytrenko@gmail.com>
2021-10-19 12:02:10 +0300

    problem with postService

--- src/Comunicazione.Core/Services/IPostService.cs
+++ src/Comunicazione.Core/Services/IPostService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comunicazione.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Comunicazione.Core.Services
 {
     public interface IPostService
     {
+        void AddPost(int userId, Post post);
+        Post GetById(int id);
     }
 }
