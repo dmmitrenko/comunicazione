53fb8303c93475e320adfbc093815c4b5a28be2d
Sergey Dmitrenko <dmmytrenko@gmail.com>
2021-11-19 21:14:27 +0300

    address validator

---
+++ src/Comunicazione.Infrastructure/Validators/AddressValidator.cs
@@ -0,0 +1,20 @@
+﻿using Comunicazione.Core.Views.Adrresses;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comunicazione.Infrastructure.Validators
+{
+    public class AddressValidator : AbstractValidator<AddressViewModelForCreation>
+    {
+        public AddressValidator()
+        {
+            RuleFor(m => m.Country).NotEmpty().Matches(@"^[a-zA-Z]+$");
+            RuleFor(m => m.City).Matches(@"^[a-zA-Z]+$");
+            RuleFor(m => m.City).Matches(@"^[a-zA-Z]+$");
+        }
+    }
+}
